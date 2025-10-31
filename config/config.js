b324613edcd807467a5c85fed5a8d6f4eca8aec9
0kana0 <cyvery2543@gmail.com>
2025-10-31 11:51:30 +0700

    เพิ่ม uat

--- config/config.js
+++ config/config.js
@@ -9,12 +9,13 @@ module.exports = {
     "port": process.env.DB_PORT,
     "dialect": "postgres"
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  "uat": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": "postgres"
   },
   "production": {
     "username": "root",
